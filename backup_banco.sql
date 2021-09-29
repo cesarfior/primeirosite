871caff177fffe88bd2cf62064ed32bf1caba399
César Fior <cesar.a.fior@hotmail.com>
2021-09-28 22:05:04 -0300

    mais um teste2

---
+++ backup_banco.sql
@@ -0,0 +1,14 @@
+ O volume na unidade C n∆o tem nome.
+ O N£mero de SÇrie do Volume Ç C6DD-5CAA
+
+ Pasta de C:\wamp64\www\modulogit
+
+28/09/2021  21:34    <DIR>          .
+28/09/2021  21:34    <DIR>          ..
+28/09/2021  21:34                 0 backup_banco.sql
+28/09/2021  20:20                10 Index.html
+28/09/2021  21:30                12 java .jar
+28/09/2021  21:00               111 readme.txt
+27/09/2021  22:07               500 style.css
+               5 arquivo(s)            633 bytes
+               2 pasta(s)   42.720.288.768 bytes dispon°veis
